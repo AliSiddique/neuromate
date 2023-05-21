fff393fc500b21180dc0a21466790edb1951f2c0
AliSiddique <alisiddique10@hotmail.com>
2023-05-21 17:41:20 +0100

    frosted navbar

--- src/app/components/3D/Models.tsx
+++ src/app/components/3D/Models.tsx
@@ -1,23 +1,27 @@
 "use client"
-import React from 'react';
+import React, { memo } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 import { OrbitControls } from '@react-three/drei';
 
 interface Props {
-  scale?: number; // Make the 'scale' prop optional
+  scale?: number;
 }
 
-export default function Models({ scale = 0.35 }: Props) {
+function Models({ scale = 0.35 }: Props) {
   const { scene } = useGLTF('/hamburger.glb');
 
   return (
     <div className="h-screen">
       <Canvas>
-        <OrbitControls />
+        <OptimizedOrbitControls />
         <ambientLight intensity={1} />
         <primitive zoom={-5} object={scene} scale={scale} />
       </Canvas>
     </div>
   );
 }
+
+const OptimizedOrbitControls = memo(OrbitControls);
+
+export default memo(Models);
--- src/app/components/Constants/Navbar.tsx
+++ src/app/components/Constants/Navbar.tsx
@@ -12,22 +12,22 @@ function classNames(...classes:any[]) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-white shadow">
+    <Disclosure as="nav" className="sticky top-0 z-50 bg-white border-b border-gray-200 shadow backdrop-filter backdrop-blur-lg bg-opacity-30">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
-            <div className="flex h-16 justify-between">
+          <div className="px-2 mx-auto max-w-7xl sm:px-4 lg:px-8">
+            <div className="flex justify-between h-16">
               <div className="flex px-2 lg:px-0">
-                <div className="flex flex-shrink-0 items-center">
+                <div className="flex items-center flex-shrink-0">
                   <Image
-                    className="block h-8 w-auto lg:hidden"
+                    className="block w-auto h-8 lg:hidden"
                     src="/smallbrain.png"
                     alt="Your Company"
                     width={32}
                     height={32}
                   />
                   <Image
-                    className="hidden h-8 w-auto lg:block"
+                    className="hidden w-auto h-8 lg:block"
                     src="/smallbrain.png"
                     alt="Your Company"
                     width={32}
@@ -37,38 +37,38 @@ export default function Navbar() {
                 <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
                   <Link
                     href="/"
-                    className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500"
                   >
                     Dashboard
                   </Link>
                   <Link
                     href="profile"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
                   >
                     Team
                   </Link>
                   <Link
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
                   >
                     Projects
                   </Link>
                   <Link
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
                   >
                     Calendar
                   </Link>
                 </div>
               </div>
-              <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
+              <div className="flex items-center justify-center flex-1 px-2 lg:ml-6 lg:justify-end">
                 <div className="w-full max-w-lg lg:max-w-xs">
                   <label htmlFor="search" className="sr-only">
                     Search
                   </label>
                   <div className="relative">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                      <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <MagnifyingGlassIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
                     </div>
                     <input
                       id="search"
@@ -82,12 +82,12 @@ export default function Navbar() {
               </div>
               <div className="flex items-center lg:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
@@ -95,48 +95,48 @@ export default function Navbar() {
        
 
                 {/* Profile dropdown */}
-                <Link className='rounded bg-indigo-50 px-2 py-1 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100' href={`/users/login`}>Login</Link>
+                <Link className='px-2 py-1 text-sm font-semibold text-indigo-600 rounded shadow-sm bg-indigo-50 hover:bg-indigo-100' href={`/users/login`}>Login</Link>
               </div>
             </div>
           </div>
 
           <Disclosure.Panel className="lg:hidden">
-            <div className="space-y-1 pb-3 pt-2">
+            <div className="pt-2 pb-3 space-y-1">
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50"
               >
                 Dashboard
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-600 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
               >
                 Team
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-600 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
               >
                 Projects
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-600 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
               >
                 Calendar
               </Disclosure.Button>
             </div>
-            <div className="border-t border-gray-200 pb-3 pt-4">
+            <div className="pt-4 pb-3 border-t border-gray-200">
               <div className="flex items-center px-4">
                 <div className="flex-shrink-0">
                   <Image
-                    className="h-10 w-10 rounded-full"
+                    className="w-10 h-10 rounded-full"
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     alt=""
                   />
@@ -147,10 +147,10 @@ export default function Navbar() {
                 </div>
                 <button
                   type="button"
-                  className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="flex-shrink-0 p-1 ml-auto text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  <BellIcon className="w-6 h-6" aria-hidden="true" />
                 </button>
               </div>
               <div className="mt-3 space-y-1">
