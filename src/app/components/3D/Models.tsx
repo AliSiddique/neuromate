5e393fb8610cf1091ae13c43e7d9984e170df766
AliSiddique <alisiddique10@hotmail.com>
2023-05-21 12:41:54 +0100

    profile

--- src/app/components/3D/Models.tsx
+++ src/app/components/3D/Models.tsx
@@ -1,22 +1,22 @@
-"use client"
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
-import {OrbitControls} from '@react-three/drei'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 
 interface Props {
-  scale: number
+  scale?: number; // Make the 'scale' prop optional
 }
 
-export default function Models({scale = 0.35}: Props) {
-    const {scene} = useGLTF('/hamburger.glb')
+export default function Models({ scale = 0.35 }: Props) {
+  const { scene } = useGLTF('/hamburger.glb');
+
   return (
-    <div className='h-screen'>
-        <Canvas>
-            <OrbitControls />
-            <ambientLight intensity={1} />
-            <primitive zoom={-5} object={scene} scale={scale} />
-        </Canvas>
+    <div className="h-screen">
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={1} />
+        <primitive zoom={-5} object={scene} scale={scale} />
+      </Canvas>
     </div>
-  )
+  );
 }
